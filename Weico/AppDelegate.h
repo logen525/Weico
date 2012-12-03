4421a5311d475ccda0f82f575ec61df052ebacfe
qianfeng <qianfeng@localhost.(none)>
2012-12-03 11:06:42 +0800

    Initial Commit

---
+++ Weico/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Weico
+//
+//  Created by qianfeng on 12-12-3.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
